fa74e42141bc2adb154f8bcfdeaa81aee2b9de61
Kyle Hale <kh@u.northwestern.edu>
2014-03-18 13:43:33 -0500

    add string/mem utility functions

--- include/string.h
+++ include/string.h
@@ -1,15 +1,72 @@
 #ifndef __STRING_H__
 #define __STRING_H__
 
-static size_t 
-strlen (const char * str)
-{
-    size_t ret = 0;
-    while (str[ret] != 0) {
-        ret++;
-    }
+#define USE_NAUT_BUILTINS 1
 
-    return ret;
-}
+
+#define isspace(c)      (c == ' ' || c == '\f' || c == '\n' || c == '\r' || c == '\t' || c == '\v')
+#define isascii(c)  (((c) & ~0x7f) == 0)
+#define isupper(c)  ((c) >= 'A' && (c) <= 'Z')
+#define islower(c)  ((c) >= 'a' && (c) <= 'z')
+#define isalpha(c)  (isupper(c) || islower(c))
+#define isdigit(c)  ((c) >= '0' && (c) <= '9')
+#define isxdigit(c) (isdigit(c) \
+                      || ((c) >= 'A' && (c) <= 'F') \
+                      || ((c) >= 'a' && (c) <= 'f'))
+#define isprint(c)  ((c) >= ' ' && (c) <= '~')
+
+#define toupper(c)  ((c) - 0x20 * (((c) >= 'a') && ((c) <= 'z')))
+#define tolower(c)  ((c) + 0x20 * (((c) >= 'A') && ((c) <= 'Z')))
+
+#ifdef USE_NAUT_BUILTINS
+void * memcpy (void * dst, const void * src, size_t n);
+int memcmp (const void * s1_, const void * s2_, size_t n);
+void * memset (void * dst, char c, size_t n);
+
+size_t strlen (const char * str);
+size_t strnlen (const char * str, size_t max);
+int strcmp (const char * s1, const char * s2);
+int strcasecmp (const char * s1, const char * s2);
+int strncmp (const char * s1, const char * s2, size_t limit);
+int strncasecmp (const char * s1, const char * s2, size_t limit);
+char * strcat (char * s1, const char * s2);
+char * strncat (char * s1, const char * s2, size_t limit);
+char * strcpy (char * dest, const char * src);
+char * strncpy (char * dest, const char * src, size_t limit);
+char * strchr (const char * s, int c);
+char * strrchr (const char * s, int c);
+char * strpbrk (const char * s, const char * accept);
+size_t strspn (const char * s, const char * accept);
+size_t strcspn (const char * s, const char * reject);
+char * strstr (const char * haystack, const char * needle);
+
+#else
+
+#define memcpy  __builtin_memcpy
+#define memset  __builtin_memset
+#define memcmp  __builtin_memcmp
+#define strlen  __builtin_strlen
+#define strnlen __builtin_strnlen
+#define strcmp  __builtin_strcmp
+#define strncmp __builtin_strncmp
+#define strcat  __builtin_strcat
+#define strncat __builtin_strncat
+#define strstr  __builtin_strstr
+#define strspn  __builtin_strspn
+#define strcspn __builtin_strcspn
+#define strchr  __builtin_strchr
+#define strrchr __builtin_strrchr
+#define strpbrk __builtin_strpbrk
+
+#endif
+
+int atoi (const char * buf);
+int strtoi (const char * nptr, char ** endptr);
+uint64_t atox (const char * buf);
+uint64_t strtox (const char * nptr, char ** endptr);
+void str_toupper (char * s);
+void str_tolower (char * s);
+
+
 
 #endif
---
+++ string.c
@@ -0,0 +1,459 @@
+/**
+ * Kyle C. Hale 2014
+ * string.h
+ *
+ * utility functions, we'll have these here just in case we need them
+ *
+ */
+
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+#include <types.h>
+
+#ifdef USE_NAUT_BUILTINS
+size_t 
+strlen (const char * str)
+{
+    size_t ret = 0;
+    while (str[ret] != 0) {
+        ret++;
+    }
+
+    return ret;
+}
+
+
+size_t
+strnlen (const char * str, size_t max)
+{
+    size_t ret = 0;
+    while (max-- && str[ret] != 0) {
+        ret++;
+    }
+    return ret;
+}
+
+
+void *
+memcpy (void * dst, const void * src, size_t n)
+{
+    unsigned char * d = (unsigned char *)dst;
+    unsigned char * s = (unsigned char *)src;
+
+    while (n--) {
+        *d++ = *s++;
+    }
+
+    return dst;
+}
+
+
+void * 
+memset (void * dst, char c, size_t n)
+{
+    unsigned char * d = (unsigned char *)dst;
+    while (n--) {
+        *d++ = c;
+    }
+
+    return dst;
+}
+        
+
+int 
+memcmp (const void * s1_, const void * s2_, size_t n) {
+    const char * s1 = s1_;
+    const char * s2 = s2_;
+
+    while (n > 0) {
+    int cmp = (*s1 - *s2);
+
+    if (cmp != 0) {
+        return cmp;
+    }
+
+    ++s1;
+    ++s2;
+    }
+
+    return 0;
+}
+
+
+int 
+strcmp (const char * s1, const char * s2) 
+{
+    while (1) {
+    int cmp = (*s1 - *s2);
+
+    if ((cmp != 0) || (*s1 == '\0') || (*s2 == '\0')) {
+        return cmp;
+    }
+
+    ++s1;
+    ++s2;
+    }
+}
+
+
+int 
+strcasecmp (const char * s1, const char * s2) 
+{
+    while (1) {
+    int cmp = (tolower(*s1) - tolower(*s2));
+
+    if ((cmp != 0) || (*s1 == '\0') || (*s2 == '\0')) {
+        return cmp;
+    }
+
+    ++s1;
+    ++s2;
+    }
+}
+
+
+int 
+strncmp (const char * s1, const char * s2, size_t limit) 
+{
+    size_t i = 0;
+
+    while (i < limit) {
+    int cmp = (*s1 - *s2);
+
+    if ((cmp != 0) || (*s1 == '\0') || (*s2 == '\0')) {
+        return cmp;
+    }
+
+    ++s1;
+    ++s2;
+    ++i;
+    }
+
+    /* limit reached and equal */
+    return 0;
+}
+
+
+int 
+strncasecmp (const char * s1, const char * s2, size_t limit) 
+{
+    size_t i = 0;
+
+    while (i < limit) {
+    int cmp = (tolower(*s1) - tolower(*s2));
+
+    if ((cmp != 0) || (*s1 == '\0') || (*s2 == '\0')) {
+        return cmp;
+    }
+
+    ++s1;
+    ++s2;
+    ++i;
+    }
+
+    return 0;
+}
+
+
+char * 
+strcat (char * s1, const char * s2) 
+{
+    char * t1 = s1;
+
+    while (*s1) { s1++; }
+    while (*s2) { *s1++ = *s2++; }
+
+    *s1 = '\0';
+
+    return t1;
+}
+
+
+char * 
+strncat (char * s1, const char * s2, size_t limit) 
+{
+    size_t i = 0;
+    char * t1;
+
+    t1 = s1;
+
+    while (*s1) { s1++; }
+
+    while (i < limit) {
+    if (*s2 == '\0') {
+        break;
+    }
+    *s1++ = *s2++;
+    }
+    *s1 = '\0';
+    return t1;
+}
+
+
+char * 
+strcpy (char * dest, const char * src)
+{
+    char *ret = dest;
+
+    while (*src) {
+        *dest++ = *src++;
+    }
+    *dest = '\0';
+
+    return ret;
+}
+
+
+char * 
+strncpy (char * dest, const char * src, size_t limit) 
+{
+    char * ret = dest;
+
+    while ((*src != '\0') && (limit > 0)) {
+    *dest++ = *src++;
+    --limit;
+    }
+
+    if (limit > 0)
+    *dest = '\0';
+
+    return ret;
+}
+
+
+char * 
+strchr (const char * s, int c) 
+{
+    while (*s != '\0') {
+    if (*s == c)
+        return (char *)s;
+    ++s;
+    }
+    return 0;
+}
+
+
+char * 
+strrchr (const char * s, int c) 
+{
+    size_t len = strlen(s);
+    const char * p = s + len;
+
+    while (p > s) {
+    --p;
+
+    if (*p == c) {
+        return (char *)p;
+    }
+    }
+    return 0;
+}
+
+
+char * 
+strpbrk (const char * s, const char * accept) 
+{
+    size_t setLen = strlen(accept);
+
+    while (*s != '\0') {
+    size_t i;
+    for (i = 0; i < setLen; ++i) {
+        if (*s == accept[i]) {
+        return (char *)s;
+        }
+    }
+    ++s;
+    }
+
+    return 0;
+}
+
+
+
+#endif  /* USE_NAUT_BUILTINS */
+
+int 
+atoi (const char * buf) 
+{
+    int ret = 0;
+
+    while ((*buf >= '0') && (*buf <= '9')) {
+    ret *= 10;
+    ret += (*buf - '0');
+    buf++;
+    }
+
+    return ret;
+}
+
+
+int 
+strtoi (const char * nptr, char ** endptr) 
+{
+    int ret = 0;
+    char * buf = (char *)nptr;
+
+    while ((*buf >= '0') && (*buf <= '9')) {
+    ret *= 10;
+    ret += (*buf - '0');
+
+    buf++;
+
+    if (endptr) {
+        *endptr = buf;
+    }
+    }
+
+    return ret;
+}
+
+
+uint64_t 
+atox (const char * buf) {
+    uint64_t ret = 0;
+
+    if (*(buf + 1) == 'x') {
+    buf += 2;
+    }
+
+    while (isxdigit(*buf)) {
+    ret <<= 4;
+
+    if (isdigit(*buf)) {
+        ret += (*buf - '0');
+    } else {
+        ret += tolower(*buf) - 'a' + 10;
+    }
+
+    buf++;
+    }
+
+    return ret;
+}
+
+
+uint64_t 
+strtox (const char * nptr, char ** endptr) 
+{
+    uint64_t ret = 0;
+    char * buf = (char *)nptr;
+
+    if (*(buf + 1) == 'x') {
+    buf += 2;
+    }
+
+    while (isxdigit(*buf)) {
+    ret <<= 4;
+
+    if (isdigit(*buf)) {
+        ret += (*buf - '0');
+    } else {
+        ret += tolower(*buf) - 'a' + 10;
+    }
+
+    buf++;
+
+    if (endptr) {
+        *endptr = buf;
+    }
+    }
+
+    return ret;
+}
+
+
+size_t 
+strspn (const char * s, const char * accept) 
+{
+    int match = 1;
+    int cnt = 0;
+    int i = 0;
+    int accept_len = strlen(accept);
+
+    while (match) {
+    match = 0;
+
+    for (i = 0; i < accept_len; i++) {
+        if (s[cnt] == accept[i]) {
+        match = 1;
+        cnt++;
+        break;
+        }
+    }
+    }
+
+    return cnt;
+}
+
+
+size_t 
+strcspn (const char * s, const char * reject) 
+{
+    int match = 0;
+    int cnt = 0;
+    int i = 0;
+    int reject_len = strlen(reject);
+
+    while (!match) {
+    for (i = 0; i < reject_len; i++) {
+        if (s[cnt] == reject[i]) {
+        match = 1;
+        break;
+        }
+    }
+
+    if (!match) {
+        cnt++;
+    }
+
+    }
+
+    return cnt;
+}
+
+
+char *
+strstr (const char * s1, const char * s2)
+{
+        int l1, l2;
+
+        l2 = strlen(s2);
+        if (!l2)
+                return (char *)s1;
+        l1 = strlen(s1);
+        while (l1 >= l2) {
+                l1--;
+                if (!memcmp(s1, s2, l2))
+                        return (char *)s1;
+                s1++;
+        }
+        return NULL;
+}
+
+
+void 
+str_tolower (char * s) 
+{
+    while (isalpha(*s)) {
+    if (!islower(*s)) {
+        *s = tolower(*s);
+    }
+    s++;
+    }
+}
+
+
+void 
+str_toupper (char * s) 
+{
+    while (isalpha(*s)) {
+    if (!isupper(*s)) {
+        *s = toupper(*s);
+    }
+    s++;
+    }
+}
+
+
+
