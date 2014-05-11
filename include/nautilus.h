510d482f3f135682a71f4f13e8e4ba6d11b378cd
Kyle Hale <kh@u.northwestern.edu>
2014-05-10 20:44:58 -0500

    update debug/error prints

--- include/nautilus.h
+++ include/nautilus.h
@@ -4,10 +4,7 @@
 #include <printk.h>
 #include <types.h>
 
-//#define DEBUG_PRINT(fmt, args...)   printk("DEBUG: " fmt, ##args)
-//#define ERROR_PRINT(fmt, args...)   printk("ERROR: " fmt, ##args)
-#define DEBUG_PRINT(fmt, args...)   printk((fmt), ##args)
-#define ERROR_PRINT(fmt, args...)   printk((fmt), ##args)
-//define PrintError(vm, vcore, fmt, args...)  V3_Print(vm, vcore, "ERROR at %s(%d): " fmt, __FILE__, __LINE__, ##args)
+#define DEBUG_PRINT(fmt, args...)   printk("DEBUG: " fmt, ##args)
+#define ERROR_PRINT(fmt, args...)   printk("ERROR at %s(%d): " fmt, __FILE__, __LINE__, ##args)
 
 #endif
