d70cabd7e7bd96a990624d9dd1b10d862bd0e086
Kyle Hale <kh@u.northwestern.edu>
2014-07-11 19:17:08 -0500

    add PIT driver

--- include/dev/timer.h
+++ include/dev/timer.h
@@ -2,9 +2,21 @@
 #define __TIMER_H__
 
 #define TIMER_IRQ 0
+#define NUM_TIMERS 1
+
+#define TEVENT_WAITING 0
+#define TEVENT_READY   1
 
 struct naut_info;
 
+struct timer_event {
+    uint8_t active;
+    uint32_t ticks;
+    volatile uint8_t event_flag;
+};
+
+
 int timer_init (struct naut_info * naut);
+void sleep (uint_t msec);
 
 #endif
--- src/dev/timer.c
+++ src/dev/timer.c
@@ -4,32 +4,129 @@
 
 #include <dev/timer.h>
 
-// NAUT_CONFIG_HZ
+#include <lib/liballoc.h>
+
+/* TODO: FIX THIS, (priv data for irq handlers) */
+static struct naut_info * timer_naut = NULL;
+
+
+static inline void 
+timer_init_event (struct timer_event * t, uint_t delay)
+{
+    t->ticks  = delay;
+    t->active = 1;
+}
+
+
+static inline void
+timer_clear_event (struct timer_event * t)
+{
+    t->ticks  = 0;
+    t->active = 0;
+}
+
+
+static void 
+timer_wait_event (struct timer_event * te)
+{
+    te->event_flag = TEVENT_WAITING;
+
+    while (1) {
+        cli();
+        if (te->event_flag == TEVENT_READY) {
+            sti();
+            break;
+        }
+        sti();
+        asm volatile ("pause");
+    }
+}
+
+
+static void
+timer_notify_event (struct timer_event * te) 
+{
+    while (!__sync_bool_compare_and_swap(&te->event_flag, 
+                                     TEVENT_WAITING,
+                                     TEVENT_READY));
+}
 
 static int
 timer_handler (excp_entry_t * excp, excp_vec_t vec)
 {
-    printk("Timer Interrupt!\n");
+    int i;
+    struct naut_info * naut = timer_naut;
+    struct timer_event * te = naut->sys->time_events;
+
+    for (i = 0; i < naut->sys->num_tevents; i++) {
+        if (te[i].active && te[i].ticks > 0) {
+            te[i].ticks--;
+            if (te[i].ticks == 0) {
+                timer_notify_event(&te[i]);
+            } 
+        }
+    }
+
     IRQ_HANDLER_END();
     return 0;
+}
+
+
+static struct timer_event * 
+find_avail_timer_event (struct naut_info * naut)
+{
+    int i;
+    for (i = 0; i < naut->sys->num_tevents; i++) {
+        if (!naut->sys->time_events[i].active) {
+            return &(naut->sys->time_events[i]);
+        }
+    }
+
+    return NULL;
+}
+
+
+void 
+sleep (uint_t msec) 
+{
+    struct timer_event * t;
+    if ((t = find_avail_timer_event(timer_naut)) == NULL) {
+        return;
+    }
+
+    timer_init_event(t, msec);
+    timer_wait_event(t);
+    timer_clear_event(t);
 }
 
 
 int 
 timer_init (struct naut_info * naut)
 {
-    uint16_t foo = 1193182L / NAUT_CONFIG_HZ;
+    uint16_t hz;
+
+    naut->sys->num_tevents = NUM_TIMERS;
+    naut->sys->time_events = malloc(NUM_TIMERS*sizeof(struct timer_event));
+    if (!naut->sys->time_events) {
+        ERROR_PRINT("Could not allocate timers\n");
+        return -1;
+    }
+    memset(naut->sys->time_events, 0, NUM_TIMERS*sizeof(struct timer_event));
+
+    hz = 1193182UL / NAUT_CONFIG_HZ;
 
     printk("Initializing 8254 PIT\n");
 
     outb(0x36, 0x43);         // channel 0, LSB/MSB, mode 3, binary
-    outb(foo & 0xff, 0x40);   // LSB
-    outb(foo >> 8, 0x40);     // MSB
+    outb(hz & 0xff, 0x40);   // LSB
+    outb(hz >> 8, 0x40);     // MSB
 
-    if (register_irq_handler(TIMER_IRQ, timer_handler) < 0) {
+    if (register_irq_handler(TIMER_IRQ, timer_handler, NULL) < 0) {
         ERROR_PRINT("Could not register timer interrupt handler\n");
         return -1;
     }
+
+    timer_naut = naut;
 
     return 0;
 }
