35bf984bb2f4a2b7f5332c12fad2e43e2d203935
Kyle Hale <kh@u.northwestern.edu>
2014-09-15 16:40:38 -0500

    keyboard debugging

--- include/dev/kbd.h
+++ include/dev/kbd.h
@@ -1,6 +1,11 @@
 #ifndef __KBD_H__
 #define __KBD_H__
 
+#define KBD_CMD_REG  0x64
+#define KBD_DATA_REG 0x60
+
+#define STATUS_OUTPUT_FULL 0x1
+
 struct naut_info;
 
 int kbd_init(struct naut_info * naut);
--- src/dev/kbd.c
+++ src/dev/kbd.c
@@ -2,12 +2,29 @@
 #include <irq.h>
 #include <dev/kbd.h>
 
+#ifndef NAUT_CONFIG_DEBUG_KBD
+#undef DEBUG_PRINT
+#define DEBUG_PRINT(fmt, args...) 
+#endif
+
 static int 
 kbd_handler (excp_entry_t * excp, excp_vec_t vec)
 {
+
+    uint8_t status, scan;
+
+    status = inb(KBD_CMD_REG);
+
+    io_delay();
+
+    if ((status & STATUS_OUTPUT_FULL) != 0) {
+      scan  = inb(KBD_DATA_REG);
+      DEBUG_PRINT("Keyboard: status=0x%x, scancode=0x%x\n", status, scan);
+      io_delay();
+    }
+
+
     IRQ_HANDLER_END();
-    
-    printk("kbd interrupt!\n");
     return 0;
 }
 
