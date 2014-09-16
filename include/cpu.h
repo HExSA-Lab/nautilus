71503bec45c6e5d2e1b8011d55ac94543e1ccd1b
Kyle Hale <kh@u.northwestern.edu>
2014-09-16 18:34:39 -0500

    bug fix for rdtsc function

--- include/cpu.h
+++ include/cpu.h
@@ -167,7 +167,7 @@ static inline uint64_t
 rdtsc (void)
 {
     uint32_t lo, hi;
-    asm volatile("rdtscll" : "=a"(lo), "=d"(hi));
+    asm volatile("rdtsc" : "=a"(lo), "=d"(hi));
     return lo | ((uint64_t)(hi) << 32);
 }
 
