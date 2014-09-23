52d37c3306d4ca7788c4448774ed84726aa2e37e
Kyle Hale <kh@u.northwestern.edu>
2014-09-22 19:37:33 -0500

    fix page size issue and minimum allocation bug in liballoc

--- lib/liballoc.c
+++ lib/liballoc.c
@@ -1,5 +1,6 @@
 #include <lib/liballoc.h>
 #include <types.h>
+#include <paging.h>
 
 /**  Durand's Ridiculously Amazing Super Duper Memory functions.  */
 
@@ -36,8 +37,8 @@ unsigned int l_inuse = 0;			//< The amount of memory in use (malloc'ed).
 
 static int l_initialized = 0;			//< Flag to indicate initialization.	
 //static int l_pageSize  = 4096;			//< Individual page size
-static int l_pageSize  = 2097152;			//< Individual page size
-static int l_pageCount = 16;			//< Minimum number of pages to allocate.
+static int l_pageSize  = PAGE_SIZE;//2097152;			//< Individual page size
+static int l_pageCount = 1;//16;			//< Minimum number of pages to allocate.
 
 
 // ***********   HELPER FUNCTIONS  *******************************
