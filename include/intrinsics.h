d51ebddebca8f9c10cc57ff576cfa9cf85d3b5d2
Kyle Hale <kh@u.northwestern.edu>
2014-10-15 18:07:47 -0500

    add branch hints

--- include/intrinsics.h
+++ include/intrinsics.h
@@ -5,4 +5,7 @@
 #define __packed __attribute__ ((packed))
 #define __always_inline __attribute__ ((always_inline))
 
+#define unlikely(c) __builtin_expect((c), 0)
+#define likely(c)   __builtin_expect((c), 1)
+
 #endif
