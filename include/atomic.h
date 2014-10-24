ee3fbcdfd43e99c97065334a9845aa166965353a
Kyle Hale <kh@u.northwestern.edu>
2014-10-24 11:49:08 -0500

    add a variant of atomic increment that returns new val

--- include/atomic.h
+++ include/atomic.h
@@ -1,8 +1,9 @@
 #ifndef __ATOMIC_H__
 #define __ATOMIC_H__
 
-#define atomic_add(var, val)  __sync_fetch_and_add((volatile typeof(var)*)&(var), (val))
+#define atomic_add(var, val)  __sync_fetch_and_add((volatile typeof(var)*)&(var), (val)) /* note: returns the old value */
 #define atomic_inc(var)       atomic_add((var), 1) 
+#define atomic_inc_val(var)  (atomic_add((var), 1) + 1)
 #define atomic_cmpswap(var, old, new) __sync_val_compare_and_swap((volatile typeof(var)*)&(var), (old), (new))
 
 
