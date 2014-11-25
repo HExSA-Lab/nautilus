e74223163e59bd6702822f853e403edc98640f88
Kyle Hale <kh@u.northwestern.edu>
2014-11-25 16:52:55 -0600

    add EFER macros

--- include/msr.h
+++ include/msr.h
@@ -7,6 +7,19 @@ extern "C" {
 
 #include <naut_types.h>
 
+
+#define IA32_MSR_EFER 0xc0000080
+
+#define EFER_SCE    1      // System Call Extensions (SYSCALL)
+#define EFER_LME   (1<<8)  // Long Mode Enable
+#define EFER_LMA   (1<<10) // Long Mode Active
+#define EFER_NXE   (1<<11) // No-Execute Enable
+#define EFER_SVME  (1<<12) // Secure Virtual Machine Enable
+#define EFER_LMSLE (1<<13) // Long Mode Segment Limit Enable
+#define EFER_FFXSR (1<<14) // Fast FXSAVE/FXRSTOR
+#define EFER_TCE   (1<<15) // Translation Cache Extension
+
+
 void msr_write (uint32_t msr, uint64_t data);
 uint64_t msr_read (uint32_t msr);
 
