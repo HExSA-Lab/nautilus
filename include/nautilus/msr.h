2045e7bd00fdf7d4a00c4a068384ed2bf467a291
Kyle Hale <kh@u.northwestern.edu>
2014-12-16 20:20:05 -0600

    add more MSR defs

--- include/nautilus/msr.h
+++ include/nautilus/msr.h
@@ -11,6 +11,11 @@ extern "C" {
 #define IA32_MSR_EFER 0xc0000080
 #define IA32_MISC_ENABLES 0x1a0
 
+
+#define MSR_FS_BASE 0xc0000100
+#define MSR_GS_BASE 0xc0000101
+#define MSR_KERNEL_GS_BASE 0xc0000102
+
 #define EFER_SCE    1      // System Call Extensions (SYSCALL)
 #define EFER_LME   (1<<8)  // Long Mode Enable
 #define EFER_LMA   (1<<10) // Long Mode Active
@@ -19,6 +24,37 @@ extern "C" {
 #define EFER_LMSLE (1<<13) // Long Mode Segment Limit Enable
 #define EFER_FFXSR (1<<14) // Fast FXSAVE/FXRSTOR
 #define EFER_TCE   (1<<15) // Translation Cache Extension
+
+
+#define AMD_MSR_TSC           0x00000010
+#define AMD_MSR_APIC_BASE     0x0000001b
+#define AMD_MSR_MPERF         0x000000e7
+#define AMD_MSR_APERF         0x000000e8
+#define AMD_MSR_MTRRCAP       0x000000fe
+#define AMD_MSR_SYSENTER_CS   0x00000174
+#define AMD_MSR_SYSENTER_ESP  0x00000175
+#define AMD_MSR_SYSENTER_EIP  0x00000176
+#define AMD_MSR_MCG_CAP       0x00000179
+#define AMD_MSR_MCG_STATUS    0x0000017a
+#define AMD_MSR_MCG_CTL       0x0000017b
+#define AMD_MSR_DEBUG_CTL     0x000001d9
+
+#define AMD_MSR_PAT           0x00000277
+#define AMD_MSR_STAR          0xc0000081
+#define AMD_MSR_LSTAR         0xc0000082
+#define AMD_MSR_CSTAR         0xc0000083
+#define AMD_MSR_SFMASK        0xc0000084
+#define AMD_MSR_FSBASE        0xc0000100
+#define AMD_MSR_GSBASE        0xc0000101
+#define AMD_MSR_KERN_GSBASE   0xc0000102
+#define AMD_MSR_TSC_AUX       0xc0000103
+
+#define AMD_MSR_SYSCFG        0xc0010010
+
+#define AMD_MSR_VM_CR         0xc0010114
+
+/* Control over Northbridge in multi-core chips */
+#define AMD_MSR_NBRIDGE_CTL   0xc001001f
 
 
 void msr_write (uint32_t msr, uint64_t data);
