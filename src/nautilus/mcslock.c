3afe6249ebcd8f43219d686aeaf0dab4f9740769
Kyle Hale <kh@u.northwestern.edu>
2015-01-10 14:56:02 -0600

    add missing MCS lock C file

---
+++ src/nautilus/mcslock.c
@@ -0,0 +1,10 @@
+#include <nautilus/nautilus.h>
+#include <nautilus/mcslock.h>
+#include <nautilus/cpu.h>
+#include <nautilus/percpu.h>
+#include <nautilus/smp.h>
+#include <nautilus/atomic.h>
+#include <nautilus/intrinsics.h>
+#include <nautilus/irq.h>
+
+
