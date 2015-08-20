cd265b9c254ce116f59026b48caaf18acd38b78c
Kyle Hale <kh@u.northwestern.edu>
2015-08-20 13:07:16 -0500

    add performance monitoring support (for now, only AMD works)

---
+++ include/nautilus/pmc.h
@@ -0,0 +1,124 @@
+#ifndef __PMC_H__
+#define __PMC_H__
+
+#include <nautilus/naut_types.h>
+
+
+#define NUM_PERF_SLOTS AMD_PERF_SLOTS
+
+#define PMC_SLOT_FREE 0
+#define PMC_SLOT_USED 1
+
+#define AMD_PERF_SLOTS    6
+#define AMD_PERF_CTL0_MSR 0xc0010200
+#define AMD_PERF_CTL1_MSR 0xc0010202
+#define AMD_PERF_CTL2_MSR 0xc0010204
+#define AMD_PERF_CTL3_MSR 0xc0010206
+#define AMD_PERF_CTL4_MSR 0xc0010208
+#define AMD_PERF_CTL5_MSR 0xc001020a
+
+#define AMD_PERF_CTR0_MSR 0xc0010201
+#define AMD_PERF_CTR1_MSR 0xc0010203
+#define AMD_PERF_CTR2_MSR 0xc0010205
+#define AMD_PERF_CTR3_MSR 0xc0010207
+#define AMD_PERF_CTR4_MSR 0xc0010209
+#define AMD_PERF_CTR5_MSR 0xc001020b
+
+#define PERF_CTL_MSR_N(n) (AMD_PERF_CTL0_MSR + 2*(n))
+#define PERF_CTR_MSR_N(n) (AMD_PERF_CTR0_MSR + 2*(n))
+
+
+/* EVENTS */
+
+#define AMD_PMC_DCACHE_MISS  0x41 // PERF_CTL[5:0]
+#define AMD_PMC_ICACHE_MISS  0x81 // PERF_CTL[2:0]
+#define AMD_PMC_L2_MISS      0x7e // PERF_CTL[2:0]
+#define AMD_PMC_TLB_MISS     0x46 // PERF_CTL[2:0]
+
+/* Counts the number of SMIs received. */
+#define AMD_PMC_SMI_CNT      0x2b // PERF_CTL[5:0]
+
+
+/* The number of cycles the instruction fetcher is stalled for the core. This
+ * may be for a variety of reasons such as branch predictor updates,
+ * unconditional branch bubbles, far jumps and cache misses, instruction
+ * fetching for the other core while instruction fetch for this core is
+ * stalled, among others. May be overlapped by instruction dispatch stalls or
+ * instruction execution, such that these stalls don't necessarily impact
+ * performance. */
+#define AMD_PMC_IFETCH_STALL 0x87 // PERF_CTL[2:0]
+
+/* The number of branch instructions retired, of any type, that were not
+ * correctly predicted.  This includes those for which prediction is not
+ * attempted (far control transfers, exceptions and interrupts).
+ */
+#define AMD_PMC_BRANCH_MISS  0xC3 // PERF_CTL[5:0]
+
+/* the number of instruction cache lines invalidated. a non-smc event is cmc
+ * (cross modify- ing code), either from the other core of the compute unit or
+ * another compute compute unit. */
+#define AMD_PMC_ICACHE_INV   0x8C // PERF_CTL[2:0]
+
+
+typedef struct event_prop {
+    const char * name;
+    uint8_t slot_mask;
+} event_prop_t;
+
+typedef struct pmc_event {
+    uint8_t assigned_idx;
+    uint8_t enabled;
+    uint8_t id;
+    event_prop_t * prop;
+} perf_event_t;
+
+
+
+typedef struct perf_slot {
+    uint8_t status;
+    perf_event_t * event;
+} perf_slot_t;
+
+
+typedef struct pmc_ctl {
+
+    union {
+        uint64_t val;
+        struct {
+            uint8_t event_select0 : 8;
+            uint8_t unit_mask     : 8;
+            uint8_t usr           : 1;
+            uint8_t os            : 1;
+            uint8_t edge          : 1;
+            uint8_t rsvd0         : 1;
+            uint8_t int_enable    : 1;
+            uint8_t rsvd1         : 1;
+            uint8_t en            : 1;
+            uint8_t inv           : 1;
+            uint8_t cnt_mask      : 8;
+            uint8_t event_select1 : 4;
+            uint8_t rsvd2         : 4;
+            uint8_t hg_only       : 2;
+            uint32_t rsvd3        : 22;
+        } __attribute__((packed));
+    } __attribute__((packed));
+} __attribute__((packed)) pmc_ctl_t;
+
+
+perf_event_t * assign_perf_event(uint8_t event_id, uint8_t unit_mask);
+void release_perf_event(perf_event_t * event);
+
+void enable_perf_event(perf_event_t * event);
+void enable_all_events(void);
+void disable_perf_event(perf_event_t * event);
+void disable_all_events(void);
+void reset_all_counters(void);
+
+uint64_t read_event_count(perf_event_t * event);
+
+int pmc_init(void);
+void perf_report(void);
+
+
+
+#endif /* !__PMC_H__! */
---
+++ src/nautilus/pmc.c
@@ -0,0 +1,304 @@
+#include <nautilus/msr.h>
+#include <nautilus/nautilus.h>
+#include <nautilus/cpu.h>
+#include <nautilus/pmc.h>
+
+#include <lib/liballoc.h>
+
+#define PMC_DEBUG(fmt, args...) DEBUG_PRINT("PMC: " fmt, ##args)
+#define PMC_INFO(fmt, args...)  printk("PMC: " fmt, ##args)
+#define PMC_ERR(fmt, args...)   ERROR_PRINT("PMC: " fmt, ##args)
+#define PMC_WARN(fmt, args...)  WARN_PRINT("PMC: " fmt, ##args)
+
+static perf_slot_t  pmc_slots[NUM_PERF_SLOTS];
+
+static event_prop_t event_props[256] = 
+{ 
+    [AMD_PMC_DCACHE_MISS]  = {"Data Cache Misses",              0x3f},
+    [AMD_PMC_ICACHE_MISS]  = {"Instruction Cache Misses",       0x07},
+    [AMD_PMC_L2_MISS]      = {"L2 Cache Misses",                0x07},
+    [AMD_PMC_TLB_MISS]     = {"Unified TLB Misses",             0x07},
+    [AMD_PMC_SMI_CNT]      = {"SMI Interrupts",                 0x3f},
+    [AMD_PMC_IFETCH_STALL] = {"Instruction Fetch Stalls",       0x07},
+    [AMD_PMC_BRANCH_MISS]  = {"Mispredicted Branches Retired",  0x3f},
+    [AMD_PMC_ICACHE_INV]   = {"Instr. Cache Lines Invalidated", 0x07},
+};
+
+
+
+static inline uint64_t
+read_pmc_ctl (uint8_t idx)
+{
+    if (idx >= NUM_PERF_SLOTS) {
+        WARN_PRINT("Attempt to access invalid PMC CTL slot (%u)\n", idx);
+        return 0;
+    }
+
+    return msr_read(PERF_CTL_MSR_N(idx));
+}
+
+static inline void
+write_pmc_ctl (uint8_t idx, uint64_t val)
+{
+    if (idx >= NUM_PERF_SLOTS) {
+        WARN_PRINT("Attempt to access invalid PMC CTL slot (%u)\n", idx);
+        return;
+    }
+
+    msr_write(PERF_CTL_MSR_N(idx), val);
+}
+
+
+static inline uint64_t
+read_pmc_ctr (uint8_t idx)
+{ 
+    if (idx >= NUM_PERF_SLOTS) {
+        PMC_WARN("Attempt to access invalid PMC CTR slot (%u)\n", idx);
+        return 0;
+    }
+
+    return msr_read(PERF_CTR_MSR_N(idx));
+
+}
+
+
+static inline void
+write_pmc_ctr (uint8_t idx, uint64_t val)
+{ 
+    if (idx >= NUM_PERF_SLOTS) {
+        PMC_WARN("Attempt to access invalid PMC CTR slot (%u)\n", idx);
+        return;
+    }
+
+    msr_write(PERF_CTR_MSR_N(idx), val);
+}
+
+
+perf_event_t * 
+assign_perf_event (uint8_t event_id, uint8_t unit_mask)
+{
+    uint8_t i;
+    perf_event_t * event = NULL;
+    perf_slot_t * slot = NULL;
+
+    for (i = 0; i < NUM_PERF_SLOTS; i++) {
+
+        slot = &pmc_slots[i];
+
+        if (slot->status == PMC_SLOT_FREE && 
+            event_props[event_id].slot_mask & (1<<i)) {
+
+            pmc_ctl_t ctl;
+
+            PMC_INFO("Assigning PMC slot %u to performance event %x (%s) with unit mask 0x%x\n", 
+                    i, 
+                    event_id,
+                    event_props[event_id].name,
+                    unit_mask);
+
+            ctl.val           = 0;
+            ctl.event_select0 = (uint8_t)event_id;
+            ctl.usr           = 1;
+            ctl.os            = 1;
+            ctl.unit_mask     = unit_mask;
+
+            slot->status = PMC_SLOT_USED;
+
+            write_pmc_ctl(i, ctl.val);
+            /* clear it just in case */
+            write_pmc_ctr(i, 0);
+
+            break;
+        }
+    }
+
+    if (i == NUM_PERF_SLOTS) {
+        PMC_ERR("Could not assign event 0x%x, no empty slots\n", event);
+        return NULL;
+    }
+
+    event = malloc(sizeof(perf_event_t));
+    if (!event) {
+        PMC_ERR("Could not allocate new perf event\n");
+        return NULL;
+    }
+
+    event->id           = event_id;
+    event->assigned_idx = i;
+    event->enabled      = 0;
+    event->prop         = &event_props[event_id];
+
+    slot->event = event;
+
+    return event;
+}
+
+
+void
+enable_perf_event (perf_event_t * event)
+{
+    pmc_ctl_t ctl;
+    ASSERT(event);
+
+    if (event->enabled) {
+        PMC_WARN("Event 0x%x is already enabled\n", event->id);
+        return;
+    }
+
+    uint8_t idx = event->assigned_idx;
+
+    event->enabled = 1;
+
+    ctl.val = read_pmc_ctl(idx);
+    ctl.en = 1;
+
+    write_pmc_ctl(idx, ctl.val);
+}
+
+
+void 
+disable_perf_event (perf_event_t * event)
+{
+    pmc_ctl_t ctl;
+    ASSERT(event);
+
+    if (event->enabled == 0) {
+        PMC_WARN("Event 0x%x is already disabled\n", event->id);
+        return;
+    }
+
+    uint8_t idx = event->assigned_idx;
+
+    event->enabled = 0;
+
+    ctl.val = read_pmc_ctl(idx);
+    ctl.en = 0;
+
+    write_pmc_ctl(idx, ctl.val);
+}
+
+
+void
+enable_all_events (void)
+{
+    unsigned i;
+    for (i = 0; i < NUM_PERF_SLOTS; i++) {
+        perf_slot_t * slot = &pmc_slots[i];
+        if (slot->status == PMC_SLOT_USED) {
+            enable_perf_event(slot->event);
+        }
+    }
+}
+
+
+void
+disable_all_events (void)
+{
+    unsigned i;
+    for (i = 0; i < NUM_PERF_SLOTS; i++) {
+        perf_slot_t * slot = &pmc_slots[i];
+        if (slot->status == PMC_SLOT_USED) {
+            disable_perf_event(slot->event);
+        }
+    }
+}
+
+
+void
+reset_all_counters (void)
+{
+    unsigned i;
+    for (i = 0; i < NUM_PERF_SLOTS; i++) {
+        write_pmc_ctr(i, 0);
+    }
+}
+
+
+void
+release_perf_event (perf_event_t * event)
+{
+    ASSERT(event);
+
+    perf_slot_t * slot = NULL;
+    uint8_t idx = event->assigned_idx;
+
+    if (idx >= NUM_PERF_SLOTS) {
+        PMC_ERR("Cannot release PMC slot for invalid index %u\n", idx);
+        return;
+    }
+
+    slot = &pmc_slots[idx];
+
+    if (slot->status == PMC_SLOT_USED) {
+        slot->status = PMC_SLOT_FREE;
+        slot->event  = NULL;
+        write_pmc_ctl(idx, 0);
+        write_pmc_ctr(idx, 0);
+    }
+
+    free(event);
+}
+
+
+uint64_t 
+read_event_count (perf_event_t * event)
+{
+    ASSERT(event);
+
+    uint8_t idx = event->assigned_idx;
+
+    return read_pmc_ctr(idx);
+}
+
+
+void
+perf_report (void)
+{
+    unsigned i;
+    perf_event_t * event = NULL;
+
+    PMC_INFO("++++++++ Perf Monitor Event Report ++++++++\n");
+
+    for (i = 0; i < NUM_PERF_SLOTS; i++) {
+
+        perf_slot_t * slot = &pmc_slots[i];
+
+        if (slot->status == PMC_SLOT_FREE) {
+            continue;
+        }
+
+        event = slot->event;
+
+        ASSERT(event);
+
+        PMC_INFO("SLOT [%u] (%s) %llu\n", 
+                i,
+                event->prop->name,
+                read_event_count(event));
+
+    }
+
+    PMC_INFO("++++++++ Perf Monitor Report End ++++++++\n");
+
+}
+
+int 
+pmc_init (void)
+{
+
+    /* TODO: make sure we actually have these counters, get the number of them from the index */
+
+    memset(pmc_slots, 0, sizeof(pmc_slots));
+
+#if 0
+    assign_perf_event(AMD_PMC_TLB_MISS, 0x77);
+    assign_perf_event(AMD_PMC_ICACHE_MISS, 0x0);
+    assign_perf_event(AMD_PMC_DCACHE_MISS, 0x1);
+    assign_perf_event(AMD_PMC_BRANCH_MISS, 0);
+    assign_perf_event(AMD_PMC_ICACHE_INV, 0);
+#endif
+
+    return 0;
+}
+
+
