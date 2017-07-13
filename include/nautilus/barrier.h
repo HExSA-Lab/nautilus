2d74d832295ae1b7250fa22f30a1812d56ecdd3a
Peter Dinda <pdinda@northwestern.edu>
2017-07-13 14:38:21 -0500

    Add counting barriers
    Signed-off-by: Kyle Hale <khale@cs.iit.edu>

--- include/nautilus/barrier.h
+++ include/nautilus/barrier.h
@@ -8,7 +8,7 @@
  * led by Sandia National Laboratories that includes several national 
  * laboratories and universities. You can find out more at:
  * http://www.v3vee.org  and
- * http://xtack.sandia.gov/hobbes
+ * http://xstack.sandia.gov/hobbes
  *
  * Copyright (c) 2015, Kyle C. Hale <kh@u.northwestern.edu>
  * Copyright (c) 2015, The V3VEE Project  <http://www.v3vee.org> 
@@ -57,6 +57,45 @@ int nk_core_barrier_raise(void);
 int nk_core_barrier_lower(void);
 int nk_core_barrier_wait(void); // waits on all other cores to arrive at barrier
 int nk_core_barrier_arrive(void); // arrive (and wait) at the core barrier
+
+
+// counting barriers
+
+typedef struct nk_counting_barrier {
+    // number of threads that must arrive at the barrier
+    // note that this can only be set when there is no barrier in progress
+    uint64_t      size;
+    uint64_t      count[2];
+    uint64_t      cur;
+} nk_counting_barrier_t;
+    
+
+static inline void nk_counting_barrier_init(nk_counting_barrier_t *b, uint64_t size)
+{
+    b->size=size; b->count[0]=b->count[1]=0; b->cur=0;
+}
+
+static inline void nk_counting_barrier(volatile nk_counting_barrier_t *b)
+{
+    uint64_t old;
+    volatile uint64_t *curp = &(b->cur);
+    long mycur = *curp;
+    volatile uint64_t *countp = &(b->count[mycur]);
+
+    old = __sync_fetch_and_add(countp,1);
+
+    if (old==(b->size-1)) {
+        // I'm the last to the party
+        *curp ^= 0x1;
+        *countp = 0;
+    } else {
+        // k1om compiler does not know what "volatile" means
+        // hence this hand-coding.
+        do {
+            __asm__ __volatile__( "pause; movq %1, %0" : "=r"(old) : "m"(*countp) : );
+        } while (old);
+    }
+}
 
 #ifdef __cplusplus
 }
