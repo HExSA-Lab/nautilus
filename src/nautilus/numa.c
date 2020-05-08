81caa9353db9a2397e8532f7667af92e90272efa
Kyle Hale <khale@cs.iit.edu>
2020-05-07 23:54:44 -0500

    bugfixes in CPU topology enumeration
    This was previously incorrectly determining socket IDs on multi-socket
    systems. Was wrong for both AMD and Intel. Both are fixed, but AMD
    only partially (will not work for Ryzen/Epyc I don't think)

--- src/nautilus/numa.c
+++ src/nautilus/numa.c
@@ -8,14 +8,14 @@
  * led by Sandia National Laboratories that includes several national 
  * laboratories and universities. You can find out more at:
  * http://www.v3vee.org  and
- * http://xtack.sandia.gov/hobbes
+ * http://xstack.sandia.gov/hobbes
  *
  * Copyright (c) 2018, Kyle C. Hale <khale@cs.iit.edu>
  * Copyright (c) 2018, The V3VEE Project  <http://www.v3vee.org> 
  *                     The Hobbes Project <http://xstack.sandia.gov/hobbes>
  * All rights reserved.
  *
- * Author: Kyle C. Hale <kh@u.northwestern.edu>
+ * Author: Kyle C. Hale <khale@cs.iit.edu>
  *
  * This is free software.  You are permitted to use,
  * redistribute, and modify it as specified in the file "LICENSE.txt".
@@ -99,8 +99,6 @@ intel_probe_with_leafb (struct nk_topo_params *tp)
 {
 	cpuid_ret_t ret;
 	unsigned subleaf = 0;
-	unsigned smt_width = 0;
-	unsigned smtcore_width = 0;
 	unsigned level_width = 0;
 	uint8_t level_type = 0;
 	uint8_t found_core = 0;
@@ -111,20 +109,21 @@ intel_probe_with_leafb (struct nk_topo_params *tp)
 		cpuid_sub(0xb, subleaf, &ret);
 		
 		// this is an invalid subleaf
-		if (ret.b == 0) {
+		if ((ret.b & 0xffff) == 0) 
 			break;
-		}
 
-		level_type  = (ret.c >> 8) & 0xff;
+		level_type  = (ret.c >> 8) & 0xffff;
 		level_width = ret.a & 0xf;
 
 		switch (level_type) {
 			case 0x1: // level type is SMT, which means level_width holds the SMT mask width
-				smt_width = level_width;
+				NUMA_DEBUG("Found smt mask width: %d\n", level_width);
+				tp->smt_mask_width = level_width;
+				tp->smt_select_mask = ~(0xffffffff << tp->smt_mask_width);
 				found_smt = 1;
 				break;
 			case 0x2: // level type is SMT + core, which means level_width holds the width of both
-				smtcore_width = level_width;
+				NUMA_DEBUG("Found smt + core mask width: %d\n", level_width);
 				found_core = 1;
 				break;
 			default:
@@ -132,17 +131,16 @@ intel_probe_with_leafb (struct nk_topo_params *tp)
 		}
 		subleaf++;
 	} while (1);
-	
-	// we saw both core and smt levels, we have hyperthreads
-	if (found_core && found_smt) {
-		tp->core_bits = smtcore_width;
-		tp->smt_bits  = smt_width;
-	} else if (!found_core && found_smt) { // only saw threads, 
-		tp->core_bits = 0;
-		tp->smt_bits  = smt_width;
-	} else {
-		NUMA_WARN("Intel chip has strange topology...\n");
-	}
+
+	tp->core_plus_mask_width = level_width;
+	tp->core_plus_select_mask = ~(0xffffffff << tp->core_plus_mask_width);
+	tp->pkg_select_mask_width = level_width;
+	tp->pkg_select_mask = 0xffffffff ^ tp->core_plus_select_mask;
+
+	if (found_smt && found_core)
+		tp->core_only_select_mask = tp->core_plus_select_mask ^ tp->smt_select_mask;
+	else 
+		NUMA_WARN("Strange Intel hardware topology detected\n");
 }
 
 static void
@@ -151,58 +149,72 @@ intel_probe_with_leaves14 (struct nk_topo_params * tp)
     cpuid_ret_t ret;
     if (cpuid_leaf_max() >= 0x4) {
         NUMA_DEBUG("Intel probing topo with leaves 1 and 4\n");
+        cpuid(1, &ret);
+        uint32_t first = next_pow2((ret.b >> 16) & 0xff);
         cpuid_sub(4, 0, &ret);
-        uint32_t eax_bits = (ret.a >> 26) & 0xff;
-        tp->smt_bits  = next_pow2(get_max_id_per_pkg());
-        tp->core_bits = next_pow2(eax_bits >> tp->smt_bits);
+        uint32_t second = ((ret.a >> 26) & 0xfff) + 1;
+        tp->smt_mask_width = ilog2(first/second);
+        tp->smt_select_mask = ~(0xffffffff << tp->smt_mask_width);
+        uint32_t core_only_mask_width = ilog2(second);
+        tp->core_only_select_mask = (~(0xffffffff << (core_only_mask_width + tp->smt_mask_width))) ^ tp->smt_select_mask;
+        tp->core_plus_mask_width = core_only_mask_width + tp->smt_mask_width;
+        tp->pkg_select_mask = 0xffffffff << tp->core_plus_mask_width;
     } else { 
-        NUMA_DEBUG("Intel probing topo using leaf 1 only\n");
-        tp->smt_bits  = next_pow2(get_max_id_per_pkg());
-        tp->core_bits = 0;
+        NUMA_ERROR("SHOULDN'T GET HERE!\n");
     }
+}
+
+static void
+amd_get_topo_legacy (struct nk_topo_params * tp)
+{
+    cpuid_ret_t ret;
+    uint32_t coreidsize;
+
+    cpuid(0x80000008, &ret);
+
+    coreidsize = (ret.c >> 12) & 0xf;
+
+    if (!coreidsize) 
+        tp->max_ncores = (ret.c & 0xff) + 1;
+    else
+        tp->max_ncores = 1 << coreidsize;
+
+    tp->max_nthreads = 1;
+}
+
+// KCH TODO: TOPOEXT NOT YET SUPPORTED. We'll likely need this for Ryzen/EPYC systems
+// We'll also need to rework this a bit due to the differing terminologies:
+// node, unit, core, thread
+static void
+amd_get_topo_topoext (struct nk_topo_params * tp)
+{
 }
 
 static void
 amd_get_topo_secondary (struct nk_topo_params * tp)
 {
     cpuid_ret_t ret;
+    uint32_t largest_extid;
+    uint8_t has_x2;
+    uint8_t has_topo_ext;
+
     NUMA_DEBUG("Attempting AMD topo probe\n");
 
-    cpuid(CPUID_AMD_BASIC_INFO, &ret);
+    cpuid(0x80000000, &ret);
+    largest_extid = ret.a;
 
-    unsigned num_cores;
-    unsigned max_num_cores;
-    unsigned threads_per_core;
-    unsigned cores_per_proc;
+    cpuid(0x1, &ret);
+    has_x2 = (ret.c >> 21) & 1;
 
-    if (ret.a >= CPUID_AMD_EXT_INFO) {
-        NUMA_DEBUG("AMD probing topo using CPUID fn 8000_0008\n");
 
-        cpuid(CPUID_AMD_EXT_INFO, &ret);
+    cpuid(0x80000001, &ret);
+    has_topo_ext = (ret.c >> 22) & 1;
 
-        tp->core_bits = (ret.c >> 12) & 0xf;
+    if (largest_extid >= 0x80000008 && !has_x2) 
+        amd_get_topo_legacy(tp);
 
-        num_cores = (ret.c & 0xff) + 1;
-
-        if (tp->core_bits) {
-            max_num_cores  = 1 << tp->core_bits;
-        } else {
-            max_num_cores = num_cores;
-        }
-
-        cores_per_proc   = num_cores;
-        threads_per_core = amd_get_cmplegacy() ? 1 : 
-                                                 get_max_id_per_pkg()/cores_per_proc;
-        NUMA_DEBUG("%u Cores per proc, %u threads per core (max logical count=%u)\n", cores_per_proc, threads_per_core, 
-                get_max_id_per_pkg());
-
-        tp->smt_bits = ilog2(threads_per_core);
-
-    } else {
-        NUMA_DEBUG("AMD probing topo using CPUID FN 0x0000_0001\n");
-        tp->core_bits = next_pow2(get_max_id_per_pkg());
-        tp->smt_bits  = 0;
-    }
+    if (has_topo_ext)
+        amd_get_topo_topoext(tp);
 }
 
 
@@ -216,64 +228,103 @@ intel_get_topo_secondary (struct nk_topo_params * tp)
 	}
 }
 
-static inline void
-get_topo_secondary (struct nk_topo_params * tp)
+
+static void
+intel_get_topo_params (struct nk_topo_params * tp)
 {
-    if (nk_is_amd()) {
-        amd_get_topo_secondary(tp);
-    } else {
+    if (has_htt()) {
         intel_get_topo_secondary(tp);
+    } else {
+        NUMA_DEBUG("No HTT support, using default values\n");
+        /* no HTT support, 1 core per package */
+        tp->core_only_select_mask = 0;
+        tp->smt_mask_width = 0;
+        tp->smt_select_mask = 0;
+        tp->pkg_select_mask = 0xffffffff;
+        tp->pkg_select_mask_width = 0;
     }
+
+    NUMA_DEBUG("Finished Intel topo probe...\n");
+    NUMA_DEBUG("\tsmt_mask_width=%d\n", tp->smt_mask_width);
+    NUMA_DEBUG("\tsmt_select_mask=0x%x\n", tp->smt_select_mask);
+    NUMA_DEBUG("\tcore_plus_mask_width=%d\n", tp->core_plus_mask_width);
+    NUMA_DEBUG("\tcore_only_select_mask=0x%x\n", tp->core_only_select_mask);
+    NUMA_DEBUG("\tcore_plus_select_mask=0x%x\n", tp->core_plus_select_mask);
+    NUMA_DEBUG("\tpkg_select_mask_width=%d\n", tp->pkg_select_mask_width);
+    NUMA_DEBUG("\tpkg_select_mask=0x%x\n", tp->pkg_select_mask);
+
+}
+
+static void
+amd_get_topo_params (struct nk_topo_params * tp)
+{
+    amd_get_topo_secondary(tp);
 }
 
 
 static void
 get_topo_params (struct nk_topo_params * tp)
 {
-    if (has_htt()) {
-
-        get_topo_secondary(tp);
-
-    } else {
-        NUMA_DEBUG("No HTT support, using default values\n");
-        /* no HTT support, 1 core per package */
-        tp->core_bits = 0;
-        tp->smt_bits  = 0;
+    if (nk_is_intel()) {
+		intel_get_topo_params(tp);
+	} else {
+		amd_get_topo_params(tp);
     }
-
-
-    NUMA_DEBUG("Finished topo probe...\n");
-    NUMA_DEBUG("\tcore_bits=0x%x\n", tp->core_bits);
-    NUMA_DEBUG("\tsmt_bits =0x%x\n", tp->smt_bits);
 }
 
-static void 
-assign_core_coords (struct cpu * me, struct nk_cpu_coords * coord, struct nk_topo_params *tp)
+
+static void
+assign_core_coords_intel (struct cpu * me, struct nk_cpu_coords * coord, struct nk_topo_params *tp)
 {
-    uint32_t my_apic_id;
+	uint32_t my_apic_id;
 
 	/* 
 	 * use the x2APIC ID if it's supported 
 	 * and this is the right environment
 	 */
-	if (!nk_is_amd() && 
-		cpuid_leaf_max() >= 0xb &&
-		has_leafb()) {
-
+	if (cpuid_leaf_max() >= 0xb && has_leafb()) {
 		cpuid_ret_t ret;
-		cpuid_sub(0x1, 0, &ret);
+		cpuid_sub(0xb, 0, &ret);
 		NUMA_DEBUG("Using x2APIC for ID\n");
 		my_apic_id = ret.d;
-
 	} else {
+        cpuid_ret_t ret;
+        cpuid_sub(0x1, 0, &ret);
+        my_apic_id = ret.b >> 24;
 		my_apic_id = apic_get_id(per_cpu_get(apic));
 	}
-		
-    coord->smt_id  = my_apic_id & ((1 << tp->smt_bits) - 1);
-    coord->core_id = (my_apic_id >> tp->smt_bits) & ((1 << tp->core_bits) - 1);
-    coord->pkg_id  = my_apic_id & ~((1 << (tp->smt_bits + tp->core_bits)) - 1);
 
-    NUMA_DEBUG("Core OS ID: %u (APIC ID=0x%x):\n", me->id, my_apic_id);
+    coord->smt_id  = my_apic_id & tp->smt_select_mask;
+    coord->core_id = (my_apic_id & tp->core_only_select_mask) >> tp->smt_mask_width;
+    coord->pkg_id  = (my_apic_id & tp->pkg_select_mask) >> tp->core_plus_mask_width;
+}
+
+
+static void
+assign_core_coords_amd (struct cpu * me, struct nk_cpu_coords * coord, struct nk_topo_params *tp)
+{
+    uint32_t my_apic_id = apic_get_id(per_cpu_get(apic));
+    uint32_t logprocid = my_apic_id % tp->max_ncores;
+
+    coord->smt_id  = my_apic_id % tp->max_nthreads;
+    coord->core_id = logprocid / tp->max_nthreads;
+    coord->pkg_id  = my_apic_id / tp->max_ncores;
+}
+
+static void 
+assign_core_coords (struct cpu * me, struct nk_cpu_coords * coord, struct nk_topo_params *tp)
+{
+
+	/* 
+	 * use the x2APIC ID if it's supported 
+	 * and this is the right environment
+	 */
+	if (nk_is_intel())
+		assign_core_coords_intel(me, coord, tp);
+	else
+		assign_core_coords_amd(me, coord, tp);
+
+    NUMA_DEBUG("Core OS ID: %u:\n", me->id);
     NUMA_DEBUG("\tLogical Core ID:  %u\n", coord->smt_id);
     NUMA_DEBUG("\tPhysical Core ID: %u\n", coord->core_id);
     NUMA_DEBUG("\tPhysical Chip ID: %u\n", coord->pkg_id);
