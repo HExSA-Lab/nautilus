a817c5b041869d898683837c3dc43ed7dbaf5bd5
Qingwei Lan <qls655@moore.wot.eecs.northwestern.edu>
2022-04-29 12:20:13 -0500

    [rust] add script to install rust toolchains

---
+++ scripts/setup_rust.sh
@@ -0,0 +1,14 @@
+# set path for qemu
+export PATH=$PATH:/home/software/qemu4/bin
+
+# install rustup
+curl https://sh.rustup.rs -sSf | sh
+
+# setup path
+source ~/.cargo/env
+
+# install nightly toolchain and compnent rust-rc
+rustup toolchain install nightly --component rust-src
+
+# install xbuild
+cargo install cargo-xbuild
